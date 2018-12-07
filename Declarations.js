ab6cb805488f0825c0ef43c9f396ac7320e08621
Brayan Espinoza <bjespinozah@gmail.com>
2018-12-07 16:21:49 -0500

    Example arrow function

--- Declarations.js
+++ Declarations.js
@@ -17,3 +17,21 @@ var plus = function (a, b) {
 }
 
 plus(4, 5);
+
+/*
+	Arrow funtions
+*/
+
+var power = (base, exponent) => {
+    let result = 1;
+    for (var i = 0; i < exponent; i++) {
+        result *= base;
+    }
+    return result;
+};
+
+var square1 = (x) => {
+    return x * x
+};
+
+var square2 = (x) => x * x;
